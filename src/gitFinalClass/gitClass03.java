bd8e18d4bb74282c0a6844ad2a8cc7407516d90a
joeactuary <goldman256@gmail.com>
2021-08-11 21:24:13 -0400

    third (None) Commit

--- src/gitFinalClass/gitClass03.java
+++ src/gitFinalClass/gitClass03.java
@@ -3,7 +3,7 @@ package gitFinalClass;
 public class gitClass03 {
 
 	public static void main(String[] args) {
-		System.out.println("this is the first commit file");
+		System.out.println("this is the none commit file");
 
 	}
 
